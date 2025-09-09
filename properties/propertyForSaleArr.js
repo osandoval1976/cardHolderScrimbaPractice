2810c678cc7746b6752bd7959101b64a11bccb48
Racso <oscarhs0576@gmail.com>
2025-09-09 19:28:44 -0400

    docs

--- properties/propertyForSaleArr.js
+++ properties/propertyForSaleArr.js
@@ -4,28 +4,28 @@ export const propertyForSaleArr = [
         priceGBP: 890000,
         roomsM2: [14, 18, 14, 10, 6],
         comment: 'Highly desirable location in stunning scenery!',
-        image: './docs/cottage.jpg'
+        image: 'cottage.jpg'
     },
     {
         propertyLocation: 'Wirral, Liverpool',
         priceGBP: 650000,
         roomsM2: [18, 16, 15, 14, 17, 19, 9, 8],
         comment: 'Astonishing view with a modern finish!',
-        image: './docs/desres.jpg'
+        image: 'desres.jpg'
     },
     {
         propertyLocation: 'Beach, Brighton',
         priceGBP: 420000,
         roomsM2: [5],
         comment: 'Beautiful interior and a spacious room.',
-        image: './docs/hut.jpg'
+        image: 'hut.jpg'
     },
     {
         propertyLocation: 'Highlands, Scotland',
         priceGBP: 550000,
         roomsM2: [6, 12, 11, 5],
         comment: 'Lots of potential, snug, a must see!',
-        image: './docs/shed.jpg'
+        image: 'shed.jpg'
     }
 ]
 
