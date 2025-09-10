f424f4b48b88b2c77ac29b276edb6b2b4f0da46c
Racso <oscarhs0576@gmail.com>
2025-09-10 19:31:46 -0400

    changed

--- properties/propertyForSaleArr.js
+++ properties/propertyForSaleArr.js
@@ -5,14 +5,14 @@ export const propertyForSaleArr = [
         priceGBP: 890000,
         roomsM2: [14, 18, 14, 10, 6],
         comment: 'Highly desirable location in stunning scenery!',
-        image: 'cottage.jpg'
+        image: '/cottage.jpg'
     },
     {
         propertyLocation: 'Wirral, Liverpool',
         priceGBP: 650000,
         roomsM2: [18, 16, 15, 14, 17, 19, 9, 8],
         comment: 'Astonishing view with a modern finish!',
-        image: 'desres.jpg'
+        image: '/desres.jpg'
     },
     {
         propertyLocation: 'Beach, Brighton',
@@ -26,7 +26,7 @@ export const propertyForSaleArr = [
         priceGBP: 550000,
         roomsM2: [6, 12, 11, 5],
         comment: 'Lots of potential, snug, a must see!',
-        image: 'shed.jpg'
+        image: '/shed.jpg'
     }
 ]
 
