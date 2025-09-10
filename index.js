a2f9a5ed2cd20fcd85d706743b7adcb0b4700c24
Racso <oscarhs0576@gmail.com>
2025-09-10 19:53:40 -0400

    changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {placeholderPropertyObj} from "./properties/placeholderPropertyObj.js"
 
 let renderHtml = []
 function getPropertyHtml(x, y=[placeholderPropertyObj]) {
-if(!x) {
+if(!x === y) {
 return x.map((addTotal) =>
 renderHtml =`
 <section class="card">
