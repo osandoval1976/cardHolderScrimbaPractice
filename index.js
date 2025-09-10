7f939aa079a7ecd0a8b0dc59fc39b96c2ba6726f
Racso <oscarhs0576@gmail.com>
2025-09-10 19:38:44 -0400

    changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if(x) {
 return x.map((addTotal) =>
 renderHtml =`
 <section class="card">
-    <img src="./${addTotal.image}">
+    <img src="/${addTotal.image}">
     <div class="card-right">
         <h2>${addTotal.propertyLocation}</h2>
         <h3>${addTotal.priceGBP}</h3>
@@ -22,7 +22,7 @@ renderHtml =`
 let [{propertyLocation, priceGBP, roomsM2,comment, image}] =y
 return renderHtml =`
 <section class="card">
-    <img src="./${image}">
+    <img src="/${image}">
     <div class="card-right">
         <h2>${propertyLocation}</h2>
         <h3>${priceGBP}</h3>
