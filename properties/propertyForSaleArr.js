209f0e9d2774d336b190229e7b7dc095b332ddac
Racso <oscarhs0576@gmail.com>
2025-09-11 19:25:25 -0400

    changed

--- properties/propertyForSaleArr.js
+++ properties/propertyForSaleArr.js
@@ -5,28 +5,28 @@ export const propertyForSaleArr = [
         priceGBP: 890000,
         roomsM2: [14, 18, 14, 10, 6],
         comment: 'Highly desirable location in stunning scenery!',
-        image: './cottage.jpg'
+        image: 'cottage.jpg'
     },
     {
         propertyLocation: 'Wirral, Liverpool',
         priceGBP: 650000,
         roomsM2: [18, 16, 15, 14, 17, 19, 9, 8],
         comment: 'Astonishing view with a modern finish!',
-        image: './desres.jpg'
+        image: 'desres.jpg'
     },
     {
         propertyLocation: 'Beach, Brighton',
         priceGBP: 420000,
         roomsM2: [5],
         comment: 'Beautiful interior and a spacious room.',
-        image: './hut.jpg'
+        image: 'hut.jpg'
     },
     {
         propertyLocation: 'Highlands, Scotland',
         priceGBP: 550000,
         roomsM2: [6, 12, 11, 5],
         comment: 'Lots of potential, snug, a must see!',
-        image: './shed.jpg'
+        image: 'shed.jpg'
     }
 ]
 
